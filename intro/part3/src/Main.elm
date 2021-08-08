e77dcc2cf934d97b1904f9bd0632eafb3e2ee707
Stanley Liu <sliu1993@hotmail.co.uk>
2021-08-08 19:04:15 +0100

    completed exercises

--- intro/part3/src/Main.elm
+++ intro/part3/src/Main.elm
@@ -36,7 +36,11 @@ update msg model =
                 { model | foo = bar }
 
     -}
-    model
+    if msg.description == "ClickedTag" then
+        { model | selectedTag = msg.data }
+
+    else
+        model
 
 
 
@@ -54,7 +58,7 @@ view model =
                     Docs for List.member: http://package.elm-lang.org/packages/elm-lang/core/latest/List#member
         -}
         articles =
-            List.filter (\article -> True)
+            List.filter (\article -> List.member model.selectedTag article.tags)
                 model.allArticles
 
         feed =
@@ -104,6 +108,7 @@ viewTag selectedTagName tagName =
     in
     button
         [ class ("tag-pill " ++ otherClass)
+        , onClick { description = "ClickedTag", data = tagName }
 
         {- 👉 TODO: Add an `onClick` handler which sends a msg
                     that our `update` function above will use
--- intro/part4/src/Main.elm
+++ intro/part4/src/Main.elm
@@ -14,27 +14,20 @@ import Html.Events exposing (onClick)
 type alias Model =
     { tags : List String
     , selectedTag : String
-
-    {- 👉 TODO: change this `allArticles` annotation to the following:
-
-        allArticles : List Article
-
-
-       💡 HINT: You'll need to move the existing annotation to a `type alias`.
-    -}
-    , allArticles :
-        List
-            { title : String
-            , description : String
-            , body : String
-            , tags : List String
-            , slug : String
-            }
+    , allArticles : List Article
     }
 
 
-{-| 👉 TODO: Replace this comment with a type annotation for `initialModel`
--}
+type alias Article =
+    { title : String
+    , description : String
+    , body : String
+    , tags : List String
+    , slug : String
+    }
+
+
+initialModel : Model
 initialModel =
     { tags = Article.tags
     , selectedTag = "elm"
@@ -52,8 +45,7 @@ type alias Msg =
     }
 
 
-{-| 👉 TODO: Replace this comment with a type annotation for `update`
--}
+update : Msg -> Model -> Model
 update msg model =
     if msg.description == "ClickedTag" then
         { model | selectedTag = msg.data }
@@ -68,6 +60,7 @@ update msg model =
 
 {-| 👉 TODO: Replace this comment with a type annotation for `view`
 -}
+view : Model -> Html Msg
 view model =
     let
         articles =
@@ -95,6 +88,7 @@ view model =
 
 {-| 👉 TODO: Replace this comment with a type annotation for `viewArticle`
 -}
+viewArticle : Article -> Html Msg
 viewArticle article =
     div [ class "article-preview" ]
         [ h1 [] [ text article.title ]
@@ -105,6 +99,7 @@ viewArticle article =
 
 {-| 👉 TODO: Replace this comment with a type annotation for `viewBanner`
 -}
+viewBanner : Html Msg
 viewBanner =
     div [ class "banner" ]
         [ div [ class "container" ]
@@ -116,6 +111,7 @@ viewBanner =
 
 {-| 👉 TODO: Replace this comment with a type annotation for `viewTag`
 -}
+viewTag : String -> String -> Html Msg
 viewTag selectedTagName tagName =
     let
         otherClass =
