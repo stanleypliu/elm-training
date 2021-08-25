9e040cc4bb63a5e0dcd638709dfabd755f1e54bb
Stanley Liu <sliu1993@hotmail.co.uk>
2021-08-25 10:21:13 +0100

    add part 8 and part 9 solutions

--- intro/part9/src/Session.elm
+++ intro/part9/src/Session.elm
@@ -1,14 +1,13 @@
-port module Session
-    exposing
-        ( Session
-        , changes
-        , cred
-        , decode
-        , login
-        , logout
-        , navKey
-        , viewer
-        )
+port module Session exposing
+    ( Session
+    , changes
+    , cred
+    , decode
+    , login
+    , logout
+    , navKey
+    , viewer
+    )
 
 import Browser.Navigation as Nav
 import Json.Decode as Decode exposing (Decoder)
@@ -84,21 +83,12 @@ logout =
     sendSessionToJavaScript Nothing
 
 
-{-| 👉 TODO 1 of 2: Replace this do-nothing function with a port that sends the
-authentication token to JavaScript.
-
-    💡 HINT 1: When you convert it to a port, the port's name _must_ match
-    the name JavaScript expects in `intro/server/public/index.html`.
-    That name is not `sendSessionToJavaScript`, so you will need to
-    rename it to match what JS expects!
-
-    💡 HINT 2: After you rename it, some code in this file will break because
-    it was depending on the old name. Follow the compiler errors to fix them!
-
--}
 sendSessionToJavaScript : Maybe String -> Cmd msg
 sendSessionToJavaScript maybeAuthenticationToken =
-    Cmd.none
+    storeSession maybeAuthenticationToken
+
+
+port storeSession : Maybe String -> Cmd msg
 
 
 
@@ -110,21 +100,12 @@ changes toMsg key =
     receiveSessionFromJavaScript (\val -> toMsg (decode key val))
 
 
-{-| 👉 TODO 2 of 2: Replace this do-nothing function with a port that receives the
-authentication token from JavaScript.
-
-    💡 HINT 1: When you convert it to a port, the port's name _must_ match
-    the name JavaScript expects in `intro/server/public/index.html`.
-    That name is not `receiveSessionFromJavaScript`, so you will need to
-    rename it to match what JS expects!
-
-    💡 HINT 2: After you rename it, some code in this file will break because
-    it was depending on the old name. Follow the compiler errors to fix them!
-
--}
 receiveSessionFromJavaScript : (Value -> msg) -> Sub msg
 receiveSessionFromJavaScript toMsg =
-    Sub.none
+    onSessionChange toMsg
+
+
+port onSessionChange : (Value -> msg) -> Sub msg
 
 
 decode : Nav.Key -> Value -> Session
